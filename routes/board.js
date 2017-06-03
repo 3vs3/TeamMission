2d3fb40dd7d97f6bfbb3a43e688e13dd6e020ee8
yida <daeun9294@gmail.com>
2017-06-04 02:10:55 +0900

    mariaDB 연동

--- routes/board.js
+++ routes/board.js
@@ -1,3 +1,13 @@
+var Client = require('mariasql');
+var db_config  = require('./config/db-config.json');
+
+var c = new Client({
+  host     : db_config.host,
+  user     : db_config.user,
+  password : db_config.password,
+  database : db_config.database
+});
+
 exports.showBoardList = function(req, res) {
-  res.send('This is board')
+  res.send('This is board');
 }
