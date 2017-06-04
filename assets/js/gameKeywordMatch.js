424bf7db3d25317d32b6031ce9512085adf5c395
bobo <https://github.com/3vs3/TeamMission>
2017-06-05 02:13:27 +0900

    키워드 매칭 게임 소스 추가

--- assets/js/gameKeywordMatch.js
+++ assets/js/gameKeywordMatch.js
@@ -116,9 +116,9 @@ $(function(){
                                 $('#divKeyword').html('');
                                 $('#divKeywordGameStart').show();
                                 gameKeywordMatch();
-                            },2);
+                            },200);
                         }
-                    }, 22*(x)+2*(y));
+                    }, 2200*(x)+200*(y));
                 })(i,j);
             }
         }
