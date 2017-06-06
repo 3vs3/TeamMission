66d5d0a75fa59d253dd3d0b6252cb3c16ac65c40
jaejin1 <opiximeo@gmail.com>
2017-06-06 22:24:06 +0900

    footer 추가, html -> pug 전환

--- routes/keyword.js
+++ routes/keyword.js
@@ -2,7 +2,12 @@ var path    = require("path");
 
 exports.matchingGame = function(req, res) {
   gMemberName = req.query.char;
+  gStep = 2;
   clearInterval(gInterval);
   gInterval = setInterval(gStartTimer, 10);
-  res.sendFile(path.join(appRoot + '/views/keywordMatch.html'));
+  global.gMemberName = gMemberName +'-mini.jpg';
+  res.render('keywordMatch', {membername:gMemberName, gStep:gStep});
+
+  console.log('gStep : ' + gStep);
+  //res.sendFile(path.join(appRoot + '/views/keywordMatch.html'));
 };
