7807bf7e2a382f0bd68f6253fa1a3b1291e6e33f
yida <daeun9294@gmail.com>
2017-06-07 02:22:50 +0900

    게시판 필수값 입력 확인

--- public/js/board.js
+++ public/js/board.js
@@ -28,8 +28,23 @@ function updateQueryStringParameter(uri, key, value) {
   var separator = uri.indexOf('?') !== -1 ? "&" : "?";
   if (uri.match(re)) {
     return uri.replace(re, '$1' + key + "=" + value + '$2');
-  }
-  else {
+  } else {
     return uri + separator + key + "=" + value;
   }
+}
+
+function validateForm() {
+  var user_name = document.forms["add-comment"]["user_name"].value;
+  if (user_name === "") {
+    alert("이름을 입력해 주세요.");
+    return false;
+  }
+
+  var comment = document.forms["add-comment"]["comment"].value;
+  if (comment === "") {
+    alert("내용을 입력해 주세요.");
+    return false;
+  }
+
+  return true;
 }
